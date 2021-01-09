6554c8bddd647830b9316e7960ce857c61a991a1
Tobias Wang <wangtao@byosoft.com.cn>
2021-01-09 00:54:46 +0000

    add files

---
+++ flash-cpld.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "obmc-flash-cpld"<< std::endl;
+    return 0;
+}
---
+++ jtag.h
@@ -0,0 +1,7 @@
+#ifndef GPIO_JTAG_H
+#define GPIO_JTAG_H
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+
+#endif
